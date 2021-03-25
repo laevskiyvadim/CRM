f2c3d8f2931e5ab39bdc0e217a84b9495ddfaa04
laevskiyvadim <laevskiy.vadim@mail.ru>
2021-03-25 22:04:19 +0300

    3.25.2021

--- ff/src/views/history.vue
+++ ff/src/views/history.vue
@@ -52,7 +52,6 @@ export default {
               return t;
             }, 0);
           }),
-          
           backgroundColor: [
             "rgba(255, 99, 132, 0.2)",
             "rgba(54, 162, 235, 0.2)",
